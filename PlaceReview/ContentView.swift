ed209bc11b5ad6519c4e38c9bded9df878905eef
Thunyathon <lostseaway@me.com>
2022-02-07 16:02:59 +0700

    Initial Commit

---
+++ PlaceReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlaceReview
+//
+//  Created by Thunyathon  Jaruchotrattanasakul on 7/2/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaceReview/PlaceReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceReviewApp.swift
+//  PlaceReview
+//
+//  Created by Thunyathon  Jaruchotrattanasakul on 7/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlaceReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
